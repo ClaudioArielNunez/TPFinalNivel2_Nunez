003e50cfc27c2e6e0e03631d952015954b33df40
ClaudioArielNunez <claudioariel_n@hotmail.com>
2023-05-08 20:16:26 -0300

    Creacion de clases y modelo de capas

---
+++ Entidades/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Articulo
+    {
+        //Atributos
+        private int Id;
+        private string Codigo;
+        private string Nombre;
+        private string Descripcion;
+        private string UrlImagen;
+        private double Precio;
+        private Marca IdMarca;
+        private Categoria IdCategoria;
+    }
+}
